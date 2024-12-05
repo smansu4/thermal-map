6e8185cb81ebfe8389a2ba6d939d4b22c3f7df9e
Smansu4 <15002688+smansu4@users.noreply.github.com>
2024-12-04 21:28:38 -0600

    draw on mouse over

---
+++ README.md
@@ -0,0 +1,9 @@
+#About
+
+This is a small project created for fun. 
+
+I wanted to create a thermal map of a hovering mouse over a blank canvas. 
+
+This project was created using:  
+- JavaFx
+- SceneBuilder
--- src/main/java/com/smansu4/thermalmap/MapController.java
+++ src/main/java/com/smansu4/thermalmap/MapController.java
@@ -2,15 +2,25 @@ package com.smansu4.thermalmap;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class MapController {
 
     @FXML
     Canvas canvas;
 
-
     @FXML
     protected void onMouseOver() {
-        //System.out.println("onMouseOver");
+        canvas.setOnMouseMoved(mouseEvent -> {
+            int x = (int) mouseEvent.getX();
+            int y = (int) mouseEvent.getY();
+
+            PixelWriter pixelWriter = canvas.getGraphicsContext2D().getPixelWriter();
+            pixelWriter.setColor(x, y, Color.ORCHID);
+        });
     }
 }
