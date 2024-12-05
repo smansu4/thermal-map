671b2255e1f4b76aad336ed6350b612fcb262756
Smansu4 <15002688+smansu4@users.noreply.github.com>
2024-12-04 22:15:36 -0600

    draw on screen with circular shape

--- src/main/java/com/smansu4/thermalmap/MapController.java
+++ src/main/java/com/smansu4/thermalmap/MapController.java
@@ -2,10 +2,7 @@ package com.smansu4.thermalmap;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.image.PixelWriter;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public class MapController {
@@ -13,14 +10,34 @@ public class MapController {
     @FXML
     Canvas canvas;
 
+    private static final int RADIUS = 10;
+
     @FXML
     protected void onMouseOver() {
         canvas.setOnMouseMoved(mouseEvent -> {
-            int x = (int) mouseEvent.getX();
-            int y = (int) mouseEvent.getY();
+            int centerX = (int) mouseEvent.getX();
+            int centerY = (int) mouseEvent.getY();
 
             PixelWriter pixelWriter = canvas.getGraphicsContext2D().getPixelWriter();
-            pixelWriter.setColor(x, y, Color.ORCHID);
+
+            int xInitialPos = centerX - RADIUS;
+            int xEndPos = centerX + RADIUS;
+            int yInitialPos = centerY - RADIUS;
+            int yEndPos = centerY + RADIUS;
+
+            for (int x = xInitialPos; x <= xEndPos; x++) {
+                for (int y = yInitialPos; y <= yEndPos; y++) {
+                    if (isInsideCircle(x, y, centerX, centerY)) {
+                        pixelWriter.setColor(x, y, Color.ORCHID);
+                    }
+                }
+            }
         });
+    }
+
+    private static boolean isInsideCircle(int x, int y, int centerX, int centerY) {
+        int distanceX = x - centerX;
+        int distanceY = y - centerY;
+        return distanceX * distanceX + distanceY * distanceY <= RADIUS * RADIUS;
     }
 }
