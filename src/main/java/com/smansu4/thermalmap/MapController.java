5e134c60ce77614f2afdf513cb19d3d05ad28c1a
Smansu4 <15002688+smansu4@users.noreply.github.com>
2024-12-04 20:42:54 -0600

    set up

---
+++ src/main/java/com/smansu4/thermalmap/MapController.java
@@ -0,0 +1,16 @@
+package com.smansu4.thermalmap;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+
+public class MapController {
+
+    @FXML
+    Canvas canvas;
+
+
+    @FXML
+    protected void onMouseOver() {
+        //System.out.println("onMouseOver");
+    }
+}
--- src/main/java/com/smansu4/thermalmap/ThermalMapApplication.java
+++ src/main/java/com/smansu4/thermalmap/ThermalMapApplication.java
@@ -7,12 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ThermalMapApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(ThermalMapApplication.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setWidth(1000);
+        stage.setHeight(700);
+        stage.setTitle("Thermal Map");
         stage.setScene(scene);
         stage.show();
     }
